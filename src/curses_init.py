5e8d31dfa0b64a4ce9a32c74bfc6ea65c1eedc31
Arthur SFEZ <arthur.sfez@gmail.com>
2014-08-22 17:46:51 +0100

    Edit box menu implemented. TODO: various checks/error handling

--- src/curses_init.py
+++ src/curses_init.py
@@ -2,7 +2,7 @@
 
 import sys
 import curses
-from curses.textpad import Textbox
+from curses.textpad import Textbox, rectangle
 import time
 import curses_menu_init
 
@@ -11,7 +11,6 @@ def init_attributes(stdscr):
   curses.noecho()
   curses.cbreak()
   curses.curs_set(0)
-  stdscr.keypad(True)
   if curses.has_colors():
     curses.start_color()
     curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLUE) # background
@@ -20,7 +19,7 @@ def init_attributes(stdscr):
     curses.init_pair(4, curses.COLOR_BLACK, curses.COLOR_CYAN) # line
 
 def clean_up_curses(stdscr):
-  stdscr.keypad(False)
+  curses.curs_set(1)
   curses.nocbreak()
   curses.echo()
   curses.endwin()
@@ -33,6 +32,7 @@ def init_backgrounds(stdscr, build_opts):
   ctn_win.bkgd(' ', curses.color_pair(2))
   stdscr.addstr(0, 1, "FreeBSD builder. Settings file: {}".\
                 format(build_opts['output_conf_file']), curses.color_pair(0))
+  stdscr.addstr(curses.LINES - 2, 0, "Press 'q' to quit and generate the settings file and the script", curses.color_pair(0))
   return (menu_win, ctn_win)
 
 def fill_menu(menu, ctn_win, pos):
@@ -46,13 +46,16 @@ def refresh_screen(windows):
   curses.doupdate()
 
 def browse_menu(c, pos, menus, cur_menu, ctn_win):
-  if c == 'q':
+  if c == ord('q'):
     return (-1, cur_menu, False)
   elif c == ord('j') or c == curses.KEY_DOWN:
     pos += 1
-  elif c == 'k':
+  elif c == ord('k') or c == curses.KEY_UP:
     pos -= 1
-  elif c == 'a':
+  elif c == 127:
+    pos = 0
+    cur_menu = "main"
+  elif  c == 10:
     if menus[cur_menu][pos][0] in menus:
       cur_menu = menus[cur_menu][pos][0]
       pos = 0
@@ -65,31 +68,32 @@ def browse_menu(c, pos, menus, cur_menu, ctn_win):
   return (pos, cur_menu, False) # (pos, editing)
 
 def validator(key):
-  if key == "c" or key == 'c' or key == ord('c'):
-    return None
-  else:
-    return key
+  if key == 127: # delete
+    return 8
+  elif key == 10:
+    return 0
+  return key
 
 def edit_field(c, menus, cur_menu, pos, ctn_win, build_opts):
+  curses.curs_set(1)
   ctn_win.clear()
-  ctn_win.addstr(0, 0, menus[cur_menu][pos][1] + "\naa", curses.color_pair(4))
-  refresh_screen([ctn_win])
-  global textbox
-  textbox = Textbox(ctn_win)
-  textbox.do_command(curses.KEY_BACKSPACE)
+  ctn_win.addstr(0, 0, menus[cur_menu][pos][1] + "\n", curses.color_pair(4))
+  txt_win = ctn_win.subwin(curses.LINES - 15, curses.COLS - 15, 7, 7)
+  txt_win.addstr(0, 0, build_opts[menus[cur_menu][pos][0]], curses.color_pair(4))
+  refresh_screen([ctn_win, txt_win])
+  textbox = Textbox(txt_win)
   textbox.edit(validator)
   value = textbox.gather()
+  curses.curs_set(0)
   build_opts[menus[cur_menu][pos][0]] = value
   return (0, "main", False)
 
 def main_event(stdscr, menu_win, ctn_win, menus, build_opts):
   pos = 0
   cur_menu = "main"
-  field = ""
   editing = False
   while True:
     c = ctn_win.getch()
-    print(c, file=sys.stderr)
     if not editing:
       (pos, cur_menu, editing) = browse_menu(c, pos, menus, cur_menu, ctn_win)
       if pos == -1:
@@ -111,5 +115,7 @@ def start_curses_gui(build_opts):
   refresh_screen([stdscr, menu_win, ctn_win])
 
   # Call loop
+  ctn_win.keypad(True)
   main_event(stdscr, menu_win, ctn_win, menus, build_opts)
+  ctn_win.keypad(False)
   clean_up_curses(stdscr)
